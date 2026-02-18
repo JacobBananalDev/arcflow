7af8ee115b3a43c4b21ea91e4d7ccac1e0017b7f
JbanzCodingLabz <jacob.r.bananal@outlook.com>
2026-02-17 21:08:18 -0800

    initialize Go Server with chi and health endpoint

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Arcflow API running"))
+	})
+
+	log.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/jacobbananaldev/arcflow
 
 go 1.25.7
+
+require github.com/go-chi/chi/v5 v5.2.5
