e536402eb20923013582a576e46c607f54d9edf7
Jacob Bananal <jacob.bananal@tek84.com>
2026-02-18 13:23:16 -0800

    add enviornment-based configuration loader and dynamic port support

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -5,15 +5,17 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/jacobbananaldev/arcflow/internal/config"
 )
 
 func main() {
 	r := chi.NewRouter()
+	cfg := config.Load()
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Arcflow API running"))
 	})
 
-	log.Println("Server running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Printf("Server running on :%s\n", cfg.Port)
+	log.Fatal(http.ListenAndServe(":"+cfg.Port, r))
 }
